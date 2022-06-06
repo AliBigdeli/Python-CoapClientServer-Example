417f26bc3cc60fa55c9bb9b5861b1b29db2ff729
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-06-06 16:45:19 +0400

    init project

---
+++ coapclient.py
@@ -0,0 +1,12 @@
+from coapthon.client.helperclient import HelperClient
+
+host = "127.0.0.1"
+port = 5683
+path ="basic"
+
+client = HelperClient(server=(host, port))
+response = client.get(path)
+print (response.pretty_print())
+client.stop()
+
+# coap://127.0.0.1:5683/big
